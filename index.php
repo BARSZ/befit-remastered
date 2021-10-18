10966e5d50908cf0bfa8c7214a8bd0fd6af3e0d9
Vasil Simeonov <vasko3107@gmail.com>
2021-10-19 00:13:19 +0300

    starting style

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <html>
 
 <head>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>
 
     </title>
 </head>
 
 <body>
-    <div class="registerForm">
-        <h1>Sign Up</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h1>Sign Up</h1>
+            </div>
+        </div>
+
         <form action="includes/signup.php" method="POST">
-            <?php
-            if (isset($_GET['username'])) {
-                $username = $_GET['username'];
-                echo '<input type="text" name="username" placeholder="Username" value="' . $username . '">';
-            } else {
-                echo '<input type="text" name="username" placeholder="Username">';
-            }
-            ?>
-            <br>
-            <input type="password" name="password" placeholder="Password">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <?php
+                    if (isset($_GET['username'])) {
+                        $username = $_GET['username'];
+                        echo '<input type="text" name="username" placeholder="Username" value="' . $username . '">';
+                    } else {
+                        echo '<input type="text" name="username" placeholder="Username">';
+                    }
+                    ?>
+                </div>
+                <div class="col-md-6 text-center">
+                    <input type="password" name="password" placeholder="Password">
+                </div>
+            </div>
             <br>
             <input type="email" name="email" placeholder="E-mail">
             <br>
@@ -56,20 +68,6 @@
             <button type="submit" name="registerButton">Sign Up</button>
         </form>
         <?php
-        /*$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        if (strpos($fullUrl, "signup=empty") == true) {
-            echo "<p class='error'>You did not fill all fields</p>";
-            exit();
-        } else if (strpos($fullUrl, "signup=invalidemail") == true) {
-            echo "<p class='error'>Invalid E-mail</p>";
-            exit();
-        } else if (strpos($fullUrl, "signup=error") == true) {
-            echo "<p class='error'>You did not press the button.</p>";
-            exit();
-        } else if (strpos($fullUrl, "signup=success") == true) {
-            echo "<p class='success'>You have been signed up.</p>";
-            exit();
-        }*/
         if (!isset($_GET['signup'])) {
             exit();
         } else {
@@ -90,8 +88,8 @@
             }
         }
         ?>
-    </div>
 
+    </div>
 </body>
 
 </html>
