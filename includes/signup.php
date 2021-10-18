96fc5aa2bebbd9d8869ce99c43a28f6971253380
Vasil Simeonov <vasko3107@gmail.com>
2021-10-18 19:05:57 +0300

    error messages for sign up page and hashing password added

--- includes/signup.php
+++ includes/signup.php
@@ -4,6 +4,7 @@ if (isset($_POST['registerButton'])) {
 
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $passwordHashed = password_hash($password, PASSWORD_DEFAULT);
     $email = $_POST['email'];
     $name = $_POST['name'];
     $age = $_POST['age'];
@@ -14,16 +15,20 @@ if (isset($_POST['registerButton'])) {
 
     if (empty($username) || empty($password) || empty($email) || empty($name) || empty($age) || empty($gender) || empty($dateOfBirth)) {
         header("Location: ../index.php?signup=empty");
+        exit();
     } else {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            header("Location: ../index.php?signup=invalidemail");
+            header("Location: ../index.php?signup=invalidemail&username=$username&name=$name&age=$age&gender=$gender&dateOfBirth=$dateOfBirth");
+            exit();
         } else {
             $sql = "INSERT INTO `clients`(`username`, `password`, `email`, `name`, `age`, `gender`, `dateOfBirth`, `credits`, `membershipExpires`) 
-                    VALUES ('$username','$password','$email','$name','$age','$gender','$dateOfBirth','$startingCredits','$membershipExpires');";
+                    VALUES ('$username','$passwordHashed','$email','$name','$age','$gender','$dateOfBirth','$startingCredits','$membershipExpires');";
             mysqli_query($conn, $sql);
             header("Location: ../index.php?signup=success");
+            exit();
         }
     }
 } else {
     header("Location: ../index.php?signup=error");
+    exit();
 }
--- index.php
+++ index.php
@@ -10,22 +10,86 @@
     <div class="registerForm">
         <h1>Sign Up</h1>
         <form action="includes/signup.php" method="POST">
-            <input type="text" name="username" placeholder="Username">
+            <?php
+            if (isset($_GET['username'])) {
+                $username = $_GET['username'];
+                echo '<input type="text" name="username" placeholder="Username" value="' . $username . '">';
+            } else {
+                echo '<input type="text" name="username" placeholder="Username">';
+            }
+            ?>
             <br>
             <input type="password" name="password" placeholder="Password">
             <br>
             <input type="email" name="email" placeholder="E-mail">
             <br>
-            <input type="text" name="name" placeholder="Name">
-            <br>
-            <input type="number" name="age" placeholder="Age">
-            <br>
-            <input type="text" name="gender" placeholder="Gender">
-            <br>
-            <input type="date" name="dateOfBirth" placeholder="Date Of Birth">
-            <br>
+            <?php
+            if (isset($_GET['name'])) {
+                $name = $_GET['name'];
+                echo '<input type="text" name="name" placeholder="Name" value="' . $name . '">';
+            } else {
+                echo '<input type="text" name="name" placeholder="Name">';
+            }
+            echo '<br>';
+            if (isset($_GET['age'])) {
+                $age = $_GET['age'];
+                echo '<input type="number" name="age" placeholder="Age" value="' . $age . '">';
+            } else {
+                echo '<input type="number" name="age" placeholder="Age">';
+            }
+            echo '<br>';
+            if (isset($_GET['gender'])) {
+                $gender = $_GET['gender'];
+                echo '<input type="text" name="gender" placeholder="Gender" value="' . $gender . '">';
+            } else {
+                echo '<input type="text" name="gender" placeholder="Gender">';
+            }
+            echo '<br>';
+            if (isset($_GET['dateOfBirth'])) {
+                $dateOfBirth = $_GET['dateOfBirth'];
+                echo '<input type="date" name="dateOfBirth" placeholder="Date Of Birth" value="' . $dateOfBirth . '">';
+            } else {
+                echo '<input type="date" name="dateOfBirth" placeholder="Date Of Birth">';
+            }
+            echo '<br>';
+            ?>
             <button type="submit" name="registerButton">Sign Up</button>
         </form>
+        <?php
+        /*$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        if (strpos($fullUrl, "signup=empty") == true) {
+            echo "<p class='error'>You did not fill all fields</p>";
+            exit();
+        } else if (strpos($fullUrl, "signup=invalidemail") == true) {
+            echo "<p class='error'>Invalid E-mail</p>";
+            exit();
+        } else if (strpos($fullUrl, "signup=error") == true) {
+            echo "<p class='error'>You did not press the button.</p>";
+            exit();
+        } else if (strpos($fullUrl, "signup=success") == true) {
+            echo "<p class='success'>You have been signed up.</p>";
+            exit();
+        }*/
+        if (!isset($_GET['signup'])) {
+            exit();
+        } else {
+            $signupCheck = $_GET['signup'];
+
+            if ($signupCheck == "empty") {
+                echo "<p class='error'>You did not fill all fields</p>";
+                exit();
+            } else if ($signupCheck == "invalidemail") {
+                echo "<p class='error'>Invalid E-mail</p>";
+                exit();
+            } else if ($signupCheck == "error") {
+                echo "<p class='error'>You did not press the button.</p>";
+                exit();
+            } else if ($signupCheck == "success") {
+                echo "<p class='success'>You have been signed up.</p>";
+                exit();
+            }
+        }
+        ?>
     </div>
 
 </body>
