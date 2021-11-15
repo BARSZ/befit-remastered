e39689e25490b29f22bfe7958c93b12f429a8363
Vasil Simeonov <vasko3107@gmail.com>
2021-11-15 18:42:35 +0200

    updated styling

--- header.php
+++ header.php
@@ -1,27 +1,32 @@
 <?php session_start(); ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
     <title>BeFit</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styles.css">
 
-<body>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">BeFit</a>
+            <a class="navbar-brand" href="home-page.php">BeFit</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="gallery-page.php">Gallery</a>
+                    <a class="nav-link" href="prices-page.php">Pricing</a>
+                    <a class="nav-link" href="profile-page.php">Profile: <?php echo $_SESSION["username"] ?></a>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a class="nav-link" href="logout-page.php">Log Out</a></li>
+                    </ul>
+                </div>
             </div>
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">Price</a></li>
-                <li><a href="#">Profile Page: <?php echo $_SESSION["username"]; ?></a></li>
-            </ul>
         </div>
     </nav>
+</head>
--- home-page.php
+++ home-page.php
@@ -1,2 +1,9 @@
 <?php
 include_once "header.php";
+?>
+
+<body class="bg-dark">
+    <img src="img/HomeBackground.jpg" class="img-fluid rounded mx-auto d-block" alt="">
+</body>
+
+</html>
